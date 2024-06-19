a497930757f7684208ab88d42bccebeb0b6e5978
Neomatrix0 <danimusci@yahoo.it>
2024-06-19 15:49:35 +0200

    metodi di console

--- Esercitazioni/Prima_esercitazione/Program.cs
+++ Esercitazioni/Prima_esercitazione/Program.cs
@@ -1,6 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, Dani!");
+
+/*Console.Write("Inserisci nome");
+Console.WriteLine(" Hello, Dani!");
+Console.Write("Inserisci nome");
+Console.Write("\nHello, Dani!\n");      //il carattere \n  serve ad andare a capo
+Console.Write("Hello, Max!\t"); 
+Console.Write("Hello, Lorenzo!\t");     //il carattere \t  serve per fare tab */
 
 //questo è un commento
 
@@ -10,3 +16,15 @@ Console.WriteLine("Hello, Dani!");
     Console.WriteLine("Hello, World!");
 
 */  
+
+///Questo è un commento per la documentazione
+
+Console.WriteLine("Premi un tasto per terminare ...");
+
+//Console.ReadKey();                        //attende la pressione di un tasto da parte dell'utente
+//string? nome = Console.ReadLine();        // legge una stringa da tastiera e la memorizza nella variabile nome
+string nome = Console.ReadLine()!;         // legge una stringa da tastiera e la memorizza nella variabile nome
+string cognome= "Musci";
+
+Console.WriteLine("Ciao," + nome + "!");   // concatenazione elementare stringhe
+Console.WriteLine($"Ciao,{nome} {cognome}!!!");      //interpolazione stringa metodo consigliato
