1883ea48ceba4024909c23c39af741f9ab231018
Neomatrix0 <danimusci@yahoo.it>
2024-06-19 12:20:45 +0200

    modifiche Program.cs

--- Esercitazioni/Prima_esercitazione/Program.cs
+++ Esercitazioni/Prima_esercitazione/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Hello, Dani!");
+
+//questo è un commento
+
+/*  questo è un commento
+    multilinea
+
+    Console.WriteLine("Hello, World!");
+
+*/  
