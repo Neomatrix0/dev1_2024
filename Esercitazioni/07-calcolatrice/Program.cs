4fd9f1ddc49fa8bcdf20cb736bda67a59883b96b
Neomatrix0 <danimusci@yahoo.it>
2024-06-21 15:57:07 +0200

    07-calcolatrice primo commit

---
+++ Esercitazioni/07-calcolatrice/Program.cs
@@ -0,0 +1,56 @@
+﻿
+Console.WriteLine("Inserire il primo numero");
+
+// Converti input primo numero in intero
+int primoNumero =Convert.ToInt32(Console.ReadLine()); 
+
+Console.WriteLine("Inserire il secondo numero");
+
+// Converti input secondo numero in intero
+int secondoNumero = Convert.ToInt32(Console.ReadLine()); 
+
+//variabili contenenti le operazioni aritmetiche
+int somma = primoNumero + secondoNumero;
+int prodotto = primoNumero * secondoNumero;
+int differenza = primoNumero - secondoNumero;
+int quoziente = primoNumero / secondoNumero;
+
+//richiede l'operatore da inserire 
+
+Console.WriteLine("scegliere l'operazione da effettuare tra + - * /");
+
+// input per inserire operatore
+string operazione = Console.ReadLine();
+
+//a seconda dell'operazione scelta verrà calcolato il risultato
+switch(operazione)
+{
+    case "*":
+    Console.WriteLine("Il risultato è " +  prodotto);
+    break;          
+
+    case "+":
+    Console.WriteLine("Il risultato è " + somma);
+    break; 
+
+
+    case "-":
+    Console.WriteLine("Il risultato è " + differenza);
+    break;  
+
+    case "/":
+    Console.WriteLine("Il risultato è " + quoziente);
+    break; 
+
+    default: 
+
+    Console.WriteLine("Inserisci l'operatore corretto");
+    break;  
+}
+
+
+
+/*if(operazione == "/" && secondoNumero ==0){
+    Console.WriteLine("Errore dividere per zero");
+}  */
+
---
+++ Esercitazioni/07-calcolatrice/README.md
@@ -0,0 +1,6 @@
+# ASSIGNMENT 3
+
+Creare una semplice calcolatrice che chieda all'utente di inserire 2 numeri (per ogni numero effettua una richiesta all'utente)
+L'app deve chiedere all'utente quale operatore vuole utilizzare (+ - / *)
+
+L'app deve restituire il risultato dell'operazione
