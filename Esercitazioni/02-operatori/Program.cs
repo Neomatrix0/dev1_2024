b115cf4d77655138a2c5348f36ac6a3653bbedda
Neomatrix0 <danimusci@yahoo.it>
2024-06-21 11:14:36 +0200

    02-operatori

---
+++ Esercitazioni/02-operatori/Program.cs
@@ -0,0 +1,29 @@
+﻿/* 
+
+Gli operatori sono utilizzati per eseguire più operazioni su uno o più operandi
+
+Gli operatori possono essere:
+
+operatori aritmetici
+operatori di confronto
+operatori logici
+operatori di assegnazione
+
+
+*/
+
+//esempio di utilizzo di operatori aritmetici
+
+int a = 10;
+
+int b = 20;
+
+int somma = a + b;          // somma = 30
+
+int differenza = a - b;     // differenza = -10
+
+int prodotto = a * b;       // prodotto = 200
+
+int divisione = a / b;      // divisione = 0
+
+int modulo = a % b;         // modulo = 10 verifica divisibilità tra 2 numeri
