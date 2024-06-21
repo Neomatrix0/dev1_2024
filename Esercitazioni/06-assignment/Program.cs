6a882e60e30fd29037c84b4ee4c64c23997794d8
Neomatrix0 <danimusci@yahoo.it>
2024-06-21 15:13:06 +0200

    06-assignment primo commit

---
+++ Esercitazioni/06-assignment/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Inserisci il numero corrispondente al giorno della settimana corrente:");
+
+int giorno = Convert.ToInt32(Console.ReadLine());  //converte il valore di input in intero altrimenti si può usare int giorno = int.Parse(Console.ReadLine())
+
+//la differenza tra Convert.ToInt32 e il Parse è che il primo restituisce 0 se il valore inserito non è numero ,mentre il secondo restituisce un eccezione
+
+switch (giorno)
+{
+    case 1:
+    Console.WriteLine("Lunedì");
+    break;          //serve per uscire dallo switch
+
+    case 2:
+    Console.WriteLine("Martedì");
+    break;  
+
+    case 3:
+    Console.WriteLine("Mercoledì");
+    break;  
+
+    case 4:
+    Console.WriteLine("Giovedì");
+    break;  
+
+    case 5:
+    Console.WriteLine("Venerdì");
+    break;  
+
+    case 6:
+    Console.WriteLine("Sabato");
+    break;  
+
+    case 7:
+    Console.WriteLine("Domenica");
+    break;
+default: 
+
+    Console.WriteLine("Il numero non corrisponde a nessun giorno della settimana");
+    break;  
+}
---
+++ Esercitazioni/06-assignment/README.md
@@ -0,0 +1,11 @@
+# ASSIGNMENT 2
+
+Creare una console app che chieda all'utente di inserire l'identificativo numerico riferito ad un giorno della settimana(numeri da 1 a 7)
+e che restituisca all'utente il giorno in stringa
+
+Per poter effettuare questa esercitazione dobbiamo eseguire una conversione da valore stringa a valore numerico intero
+utilizzando uno dei metodi di conversione:
+
+- Convert.ToInt32
+- Int.Parse
+
