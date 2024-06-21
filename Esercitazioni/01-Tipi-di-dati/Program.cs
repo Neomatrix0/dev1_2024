affbaea407812d578ec8f2c0e22646a67a9ad1b5
Neomatrix0 <danimusci@yahoo.it>
2024-06-21 10:42:42 +0200

    01-Tipi-di-dati

---
+++ Esercitazioni/01-Tipi-di-dati/Program.cs
@@ -0,0 +1,50 @@
+﻿//esempio di commento single line
+
+/*esempio di commento 
+multi line*/
+
+// esempio di dichiarazione di variabili
+
+int numero;     // dichiaro una variabile di tipo intero
+
+string nome;    // dichiaro una variabile di tipo stringa
+
+bool flag;      //dichiaro una variabile di tipo booleano
+
+
+// esempio di dichiarazione ed inizializzazione di variabili
+
+numero = 10;    //inizializzo la variabile numero con valore 10
+
+nome = "Mario"; //inizializzo la variabile nome con il valore "Mario"
+
+flag = true;    //inizializzo la variabile flag con il valore true 
+
+
+//esempio di dichiarazione e inizializzazione
+
+int eta = 20;   //dichiarazione e inizializzazione della variabile eta
+
+string cognome = "Rossi";
+
+bool isStudent = false;     //dichiarazione e inizializzazione della variabile  isStudent con valore false
+
+//esempio di dichiarazione e inizializzazione di una costante
+
+const double PI = 3.14159;  //dichiaro e inizializzo la costante PI con il valore 3.14159
+
+//esempio di dichiarazione e inizializzazione di una variabile con la parola chiave var
+
+var altezza = 1.80;     //dichiaro e inizializzo la variabile altezza con il valore 1.80
+
+// lo svantaggio è che non è possibile dichiarare una variabile var senza inizializzarla
+
+//REGOLA IMPORTANTE: una variabile deve essere inizializzata prima di essere utilizzata
+
+//REGOLA IMPORTANTE: i nomi delle variabili devono essere significativi e autoesplicativi e non usare abbreviazioni
+
+// in c# i nomi delle variabili e costanti possono essere scritti in due modi
+
+Pascal Case // NumeroIntero
+Camel Case   // numeroIntero   (la prima lettera è minuscola)
+
