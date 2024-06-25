d1c21f6f5f30918c081a3e8ee53d2ffbee42fa56
Neomatrix0 <danimusci@yahoo.it>
2024-06-25 09:30:15 +0200

    modificata per accettare float

--- Esercitazioni/07-calcolatrice/Program.cs
+++ Esercitazioni/07-calcolatrice/Program.cs
@@ -1,56 +1,63 @@
-﻿
-Console.WriteLine("Inserire il primo numero");
+﻿﻿// Calcolatrice per eseguire le 4 operazioni aritmetiche
 
-// Converti input primo numero in intero
-int primoNumero =Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Questa è una calcolatrice.Prego inserire di seguito la prima cifra");
 
-Console.WriteLine("Inserire il secondo numero");
+// riceve il primo numero come  input e lo converte nel formato float
 
-// Converti input secondo numero in intero
-int secondoNumero = Convert.ToInt32(Console.ReadLine()); 
+float? num1 = float.Parse(Console.ReadLine());
 
-//variabili contenenti le operazioni aritmetiche
-int somma = primoNumero + secondoNumero;
-int prodotto = primoNumero * secondoNumero;
-int differenza = primoNumero - secondoNumero;
-int quoziente = primoNumero / secondoNumero;
+Console.WriteLine("Prego inserire sotto la secondo cifra cifra");
 
-//richiede l'operatore da inserire 
+// riceve il secondo  numero come  input e lo converte nel formato float
 
-Console.WriteLine("scegliere l'operazione da effettuare tra + - * /");
+float? num2 = float.Parse(Console.ReadLine());
 
-// input per inserire operatore
-string operazione = Console.ReadLine();
+//chiede di scegliere il tipo di operazione da svolgere
 
-//a seconda dell'operazione scelta verrà calcolato il risultato
-switch(operazione)
-{
-    case "*":
-    Console.WriteLine("Il risultato è " +  prodotto);
-    break;          
+Console.WriteLine("Prego inserire il simbolo dell'operazione artimetica da svolgere.\n * Per la moltiplicazione, / per la divisione,+ per la somma, - per la sottrazione");
 
-    case "+":
-    Console.WriteLine("Il risultato è " + somma);
-    break; 
+// riceve come input il simbolo dell'operazione scelta
 
+string? operazione = Console.ReadLine();
 
-    case "-":
-    Console.WriteLine("Il risultato è " + differenza);
-    break;  
+// inizializza variabile risultato 
 
-    case "/":
-    Console.WriteLine("Il risultato è " + quoziente);
-    break; 
+float? risultato =0;
 
-    default: 
+// costrutto switch a seconda del tipo di simbolo scelto verrà eseguita la corrispondente operazione
 
-    Console.WriteLine("Inserisci l'operatore corretto");
-    break;  
+switch(operazione){
+    case "+": risultato = num1 + num2;
+    break;
+
+    case "-" : risultato = num1 - num2;
+    break;
+
+    case "*" :  risultato = num1 * num2;
+    break;
+
+    case "/" : 
+
+    // se il divisore è 0 la divisione è impossibile
+
+    if(num2 == 0){
+        Console.WriteLine("Mi dispiace,ma non è possibile dividere un numero per 0");
+        return;
+
+    }
+
+    // se il divisore è diverso da 0 la divisione verrà svolta
+
+    risultato = num1 / num2;
+    break;
+
+    // in caso non venga scelto il simbolo corretto apparirà il seguente emssaggio
+    default:
+     Console.WriteLine("L'operazione scelta non è corretta,prego riprovare.");
+     break; 
+
 }
 
+    // mostra il risultato finale dell'operazione scelta
 
-
-/*if(operazione == "/" && secondoNumero ==0){
-    Console.WriteLine("Errore dividere per zero");
-}  */
-
+Console.WriteLine($"Il risultato è {risultato}");
