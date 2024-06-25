6239fe6f14386fa2bcb82643954361ee94f6bd62
Neomatrix0 <danimusci@yahoo.it>
2024-06-25 12:33:16 +0200

    012-metodo-random

---
+++ Esercitazioni/012-metodo-random/Program.cs
@@ -0,0 +1,20 @@
+﻿// metodo random
+
+Random random = new Random();       //new è un costruttore 
+
+Console.WriteLine("Inserisci il primo numero dell'intervallo");
+
+// converte la stringa di input in int
+
+int primoNumero = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Inserisci il secondo numero dell'intervallo");
+
+int secondoNumero = int.Parse(Console.ReadLine());
+
+// genera numero random nell'intervallo scelto
+
+int numeroCasuale = random.Next(primoNumero, secondoNumero);
+
+Console.WriteLine($"Il numero casuale è {numeroCasuale}");
+
