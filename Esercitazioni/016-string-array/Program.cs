1c662332149c84fffe608e94282dc19e2f154882
Neomatrix0 <danimusci@yahoo.it>
2024-06-28 10:21:54 +0200

    016-string-array

---
+++ Esercitazioni/016-string-array/Program.cs
@@ -0,0 +1,28 @@
+﻿// array structure 
+
+string[] nomi = new string[3];
+
+nomi[0] = "Mario";
+nomi[1] = "Luigi";
+nomi[2] = "Giovanni";
+
+
+Console.WriteLine($"Ciao {nomi[0]},{nomi[1]},{nomi[2]}.");
+
+// ciclo for
+
+for (int i = 0; i < nomi.Length; i++)
+{
+    Console.WriteLine("Arrivederci " + nomi[i]);
+}
+
+// oppure con foreach
+
+foreach (string nome in nomi)
+{
+    Console.WriteLine(nome);
+}
+
+
+
+
