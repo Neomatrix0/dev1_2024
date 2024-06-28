638d9ba39a5b70ccf9091b231db881254f879916
Neomatrix0 <danimusci@yahoo.it>
2024-06-28 10:53:25 +0200

    19string-list

---
+++ Esercitazioni/019-list-string/Program.cs
@@ -0,0 +1,12 @@
+﻿// String List
+
+List<string> nomi = new List<string>();
+
+nomi.Add("Mario");
+nomi.Add("Luigi");
+nomi.Add("Giovanni");
+nomi.Add("Ginevra");
+
+Console.WriteLine($"Ciao {nomi[0]},{nomi[1]},{nomi[2]}.");
+
+
