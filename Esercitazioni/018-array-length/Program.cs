ae43d6927263e8ad15d9d836b5f5c6b249a6495c
Neomatrix0 <danimusci@yahoo.it>
2024-06-28 10:38:28 +0200

    18-length-array

---
+++ Esercitazioni/018-array-length/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+string[] nomi = new string[3];
+
+nomi[0] = "Mario";
+nomi[1] = "Luigi";
+nomi[2] = "Giovanni";
+
+
+Console.WriteLine($"Ciao {nomi[0]},{nomi[1]},{nomi[2]}.");
+Console.WriteLine($"Il numero degli elementi è {nomi.Length}.");
