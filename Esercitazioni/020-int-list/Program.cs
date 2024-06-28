9f64233f7bd14cbdd461863f62bcb44c15b97385
Neomatrix0 <danimusci@yahoo.it>
2024-06-28 10:57:17 +0200

    020-int-list

---
+++ Esercitazioni/020-int-list/Program.cs
@@ -0,0 +1,12 @@
+﻿// int List
+
+List<int> numbers = new List<int>();
+
+numbers.Add(10);
+numbers.Add(20);
+numbers.Add(30);
+
+
+Console.WriteLine($"The numbers are {numbers[0]},{numbers[1]},{numbers[2]}.");
+
+
