76841aa29f1ecc56d0db533502626c63fe0f3b3f
Neomatrix0 <danimusci@yahoo.it>
2024-06-28 10:30:04 +0200

    17-int-array

---
+++ Esercitazioni/017-int-array/Program.cs
@@ -0,0 +1,16 @@
+﻿// array int
+
+
+int [] numeri = new int [3];
+    numeri[0] = 10;
+    numeri[1] = 20;
+    numeri[2] = 30;
+
+
+Console.WriteLine($"I numeri sono {numeri[0]}, {numeri[1]}, {numeri[2]}.");
+
+
+foreach( int numero in numeri){
+    Console.WriteLine($"I numeri sono {numero}");
+
+}
