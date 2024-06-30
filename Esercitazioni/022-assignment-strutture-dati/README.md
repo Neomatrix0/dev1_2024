7b9e0362aaaaa349db79967bd52aaca43b18e00e
Neomatrix0 <danimusci@yahoo.it>
2024-06-30 22:06:21 +0200

    modifiche applicazione

--- Esercitazioni/022-assignment-strutture-dati/README.md
+++ Esercitazioni/022-assignment-strutture-dati/README.md
@@ -38,7 +38,7 @@ la app toglie dalla lista il nome sorteggiato e lo aggiunge ad una seconda lista
 l'app permette d'inserire un nuovo partecipante
 1. inserisci partecipante 
 
-- nome partecipante:
+    - nome partecipante:
 
 2. visualizza partecipanti
 
--- Esercitazioni/22A-assignment5/Program.cs
+++ Esercitazioni/22A-assignment5/Program.cs
@@ -8,13 +8,13 @@ List<string> listaNomi = new List<string>();
 
 while (true)
 {
-    Console.WriteLine("Digita 1 se vuoi inserire un nuovo partecipante.\nDigita 2 per vedere l'elenco dei partecipanti.\n3 per uscire\n");
+    Console.WriteLine("\nDigita 1 se vuoi inserire un nuovo partecipante.\nDigita 2 per vedere l'elenco dei partecipanti.\nDigita 3 per uscire\n");
 
-// converte un input di tipo stringa in un intero
+    // converte un input di tipo stringa in un intero
 
     int scelta = Convert.ToInt32(Console.ReadLine());
 
-// in base al tipo di scelta si può inserire un nuovo utente nella lista,visualizzare la lista o uscire dall'app
+    // in base al tipo di scelta si può inserire un nuovo utente nella lista,visualizzare la lista o uscire dall'app
 
     switch (scelta)
     {
@@ -23,6 +23,7 @@ while (true)
             Console.WriteLine("Inserisci partecipante: \n");
             string scelto = Console.ReadLine().ToLower().Trim();        // accetta una stringa come input
             listaNomi.Add(scelto);                                      // viene aggiunto il nome alla lista 
+            Thread.Sleep(1000);                                         // pausa di 1 secondo                            
             break;
 
 
@@ -30,25 +31,40 @@ while (true)
 
             Console.WriteLine("Visualizza partecipante\n");
 
-            foreach (string nome in listaNomi)                               // ciclo per mostrare tutti gli utenti aggiunti alla lista 
-            {
-                Console.WriteLine(nome);
+            // avverte se la lista è ancora vuota
 
+            if (listaNomi.Count == 0)
+            {
+                Console.WriteLine("Nessun partecipante presente.");
+            }
+            else
+            {
+
+                foreach (string nome in listaNomi)                               // ciclo per mostrare tutti gli utenti aggiunti alla lista 
+                {
+                    Console.WriteLine(nome);
+
+                }
             };
+            Thread.Sleep(1000);
+
             break;
 
         case 3:
 
             Console.WriteLine("L'applicazione si chiuderà");                    // digita 3 per uscire dal loop
+            Thread.Sleep(1000);
             return;
 
         default:
-                    Console.WriteLine("Scelta non valida. Riprova.\n");         //se vengono digitati altri numeri non compresi nelle casistiche l'applicazione lo segnala
-                    break;
-            
-            
-            
+            Console.WriteLine("Scelta non valida. Riprova.\n");         //se vengono digitati altri numeri non compresi nelle casistiche l'applicazione lo segnala
+            Thread.Sleep(1000);
+            break;
+
+
+
 
     }
 
 }
+
