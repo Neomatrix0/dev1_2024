0be5dea36fb7e26534d93361ab67a3acbea00100
Neomatrix0 <danimusci@yahoo.it>
2024-07-01 11:20:24 +0200

    modifiche

--- Esercitazioni/22A-assignment5/Program.cs
+++ Esercitazioni/22A-assignment5/Program.cs
@@ -8,7 +8,7 @@ List<string> listaNomi = new List<string>();
 
 while (true)
 {
-    Console.WriteLine("\nDigita 1 se vuoi inserire un nuovo partecipante.\nDigita 2 per vedere l'elenco dei partecipanti.\nDigita 3 per uscire\n");
+    Console.WriteLine("\nDigita 1 se vuoi inserire un nuovo partecipante.\nDigita 2 per vedere l'elenco dei partecipanti.\nDigita 3 per uscire\nDigita 4 per mettere in ordine alfabetico la lista oppure invertirla\n");
 
     // converte un input di tipo stringa in un intero
 
@@ -56,6 +56,63 @@ while (true)
             Thread.Sleep(1000);
             return;
 
+        case 4:
+          
+        /* versione ottimale
+        listaNomi.Sort();
+        Console.WriteLine();
+        ins = Console.ReadKey(true).KeyChar;
+        if( ins == 'd') listaNomi.Reverse();
+        Console.Clear();
+        break;
+
+
+
+
+         */
+            Console.WriteLine("se digiti 1 mette la lista in ordine alfabetico, se digiti 2 la lista si inverte");
+
+
+            int ordine = Convert.ToInt32(Console.ReadLine());                       // accetta input e lo converte in int
+
+            if (ordine == 1)
+            {
+
+                listaNomi.Sort();                                                   //sistema partecipanti in ordine alfabetico
+
+                Console.WriteLine("La lista in ordine alfabetico\n");
+
+                foreach (string nome in listaNomi)                                  // ciclo per mostrare tutti gli utenti aggiunti alla lista 
+                {
+
+                    Console.WriteLine(nome);
+
+                };
+                                                                                     //scelta d'invertire l'elenco
+            }
+            else if (ordine == 2)
+            {
+
+                listaNomi.Reverse();                                                 //inverte l'ordine dei partecipanti
+
+                Console.WriteLine($"\nLa lista in ordine invertito.\n");
+
+                foreach (string nome in listaNomi)                                   // ciclo per mostrare tutti gli utenti aggiunti alla lista  in modo invertito
+                {
+                    Console.WriteLine(nome);
+
+                };
+            }else{
+                Console.WriteLine("Numero errato.Puoi digitare solo 1 o 2.");
+            }
+
+
+
+
+
+            Thread.Sleep(1000);
+            break;*/
+
         default:
             Console.WriteLine("Scelta non valida. Riprova.\n");         //se vengono digitati altri numeri non compresi nelle casistiche l'applicazione lo segnala
             Thread.Sleep(1000);
@@ -68,3 +125,48 @@ while (true)
 
 }
 
+//versione insegnante
+/*
+
+List<string> partecipanti = new List<string>();
+string nome;
+int scelta;
+
+do{
+Console.WriteLine("1. Inserisci partecipante");
+Console.WriteLine("2. Visualizza partecipanti");
+Console.WriteLine("3. Esci");
+
+scelta = Convert.ToInt32(Console.ReadLine());
+
+switch(scelta){
+
+case 1:
+
+Console.Write("Nome partecipante: ");
+nome = Console.ReadLine();
+partecipanti.Add(nome);
+break;
+
+case 2:
+
+Console.WriteLine("Elenco partecipanti: ");
+foreach(string partecipante in partecipanti){
+Console.WriteLine(partecipante);
+}
+break;
+
+case 3:
+Console.WriteLine("Arrivederci");
+break;
+
+default:
+Console.WriteLine("Scelta non valida");
+break;
+}
+}
+while(scelta != 3); // il ciclo continua finchè la scelta è diversa da 3
+
+
+
+*/
---
+++ Esercitazioni/22A-assignment5/README.md
@@ -0,0 +1,11 @@
+# Versione 5
+
+l'app permette d'inserire un nuovo partecipante,vedere la lista dei partecipanti o uscire
+1. inserisci partecipante 
+
+    - nome partecipante:
+
+2. visualizza elenco partecipanti
+
+3. esci dall'applicazione
+
