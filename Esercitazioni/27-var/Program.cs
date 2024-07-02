339f995ab7b6ae3c21d8540877ba9111d6cf3d09
Neomatrix0 <danimusci@yahoo.it>
2024-07-02 11:04:34 +0200

    27-appunti-var

--- Esercitazioni/27-var/Program.cs
+++ Esercitazioni/27-var/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// esempi di var
 
 var numeri = new List<int> { 1, 2, 3, 4,5};
 
@@ -7,10 +7,15 @@ foreach ( var numero in numeri){
 }
 
 
+
 // utilizziamo var perchè non sappiamo il tipo di dato che contiene la lista
-// se fosse una lista fi stringhe avremmo dovuto scrivere List<string> numeri = new List<string> {"1","2","3"};
+// se fosse una lista di stringhe avremmo dovuto scrivere List<string> numeri = new List<string> {"1","2","3"};
 //oppure List<int> numeri = new List <int> {1,2,3}
-
+// invece utilizzando var il compilatore capisce da solo il tipo di dato
 //inoltre possiamo usare var anche per i dati anonimi
-
 //ad esempio var persona = new {Nome = "Mario", Cognome = "Rossi"};
+// qui il tipo di dato è anonimo perchè non dichiarato esplicitamente
+// ma il compilatore capisce che persona è un oggetto con 2 proprietà Nome e Cognome di tipo stringa
+// quindi possiamo scrivere persona.Nome o persona.Cognome
+// possiamo utilizzare var anche per tipi generici
+//ad esempio var numeri = new List<int> {1,2,3,4,5};
