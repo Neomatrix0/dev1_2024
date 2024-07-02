7cfc4b24aa339ceb632ac5ccd0a977570233af4e
Neomatrix0 <danimusci@yahoo.it>
2024-07-02 09:53:45 +0200

    025-registro-presenze

---
+++ Esercitazioni/25-registro-presenze/Program.cs
@@ -0,0 +1,18 @@
+﻿// registro presenze
+
+
+Dictionary<string, bool> presenze = new Dictionary<string, bool>();
+
+presenze["Mario Rossi"] = true;
+presenze["Luca Bianchi"] = false;
+
+foreach(KeyValuePair<string, bool> dipendente in presenze){
+
+    if(dipendente.Value){
+        Console.WriteLine($"Dipendente: {dipendente.Key}, Stato: Presente");
+    }else{
+        Console.WriteLine($"Dipendente: {dipendente.Key}, Stato: Assente");
+    }
+
+
+}
