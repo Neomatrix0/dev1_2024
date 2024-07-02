67bbf6c1bd7aaa2205ad5cf4de51a8a4357fcb7f
Neomatrix0 <danimusci@yahoo.it>
2024-07-02 10:32:20 +0200

    026-registro-voti

---
+++ Esercitazioni/26-registro-voti/Program.cs
@@ -0,0 +1,18 @@
+﻿// registro voti
+
+Dictionary<string, List<int>> registroClassi = new Dictionary<string, List<int>>();
+
+registroClassi["Marco"] = new List<int> { 7, 8, 9};
+
+registroClassi["Laura"] = new List<int> { 6, 7, 8};
+
+// aggiunge nuovo voto a studente Marco
+
+registroClassi["Marco"].Add(10);
+
+foreach(var studente in registroClassi){                            // var variabile generica per accogliere diversi tipi di dati
+    Console.WriteLine($"Studente: {studente.Key}, Voti: {string.Join("-",studente.Value)}");     //string.Join unisce gli elementi di una sequenza in una stringa
+}
+
+
+
