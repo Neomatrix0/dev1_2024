a4b32c2f8881b832c0feedf8138dbc2775062ebe
Neomatrix0 <danimusci@yahoo.it>
2024-07-02 10:04:25 +0200

    025-registro-presenze modifica

--- Esercitazioni/25-registro-presenze/Program.cs
+++ Esercitazioni/25-registro-presenze/Program.cs
@@ -3,16 +3,57 @@
 
 Dictionary<string, bool> presenze = new Dictionary<string, bool>();
 
-presenze["Mario Rossi"] = true;
-presenze["Luca Bianchi"] = false;
+presenze["Mario Rossi"] = false;
+presenze["Luca Bianchi"] = true;
 
-foreach(KeyValuePair<string, bool> dipendente in presenze){
 
-    if(dipendente.Value){
+
+foreach (KeyValuePair<string, bool> dipendente in presenze)
+{
+
+    // se il valore di default è true
+
+    if (dipendente.Value)
+    {
         Console.WriteLine($"Dipendente: {dipendente.Key}, Stato: Presente");
-    }else{
+    }
+    else
+    {
         Console.WriteLine($"Dipendente: {dipendente.Key}, Stato: Assente");
     }
 
 
 }
+
+// cambio stato dipendente
+
+Console.WriteLine("Di quale dipendente vuoi cambiare lo stato?");
+
+string nomeDipendente = Console.ReadLine();
+
+if (presenze.ContainsKey(nomeDipendente))
+{
+    presenze[nomeDipendente] = !presenze[nomeDipendente];   // cambio stato dipendente
+}
+else
+{
+    Console.WriteLine("Il dipendente non è presente nella lista");
+}
+
+// stampa lista aggiornata
+
+foreach (KeyValuePair<string, bool> dipendente in presenze)
+{
+
+    if (dipendente.Value)
+    {
+        Console.WriteLine($"Dipendente: {dipendente.Key}, Stato: Presente");
+    }
+    else
+    {
+        Console.WriteLine($"Dipendente: {dipendente.Key}, Stato: Assente");
+    }
+}
+
+
+
