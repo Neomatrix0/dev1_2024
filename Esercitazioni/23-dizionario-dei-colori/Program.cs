059cf05586d7de6c8793aa22eb1ba770f9be94ac
Neomatrix0 <danimusci@yahoo.it>
2024-07-02 09:37:30 +0200

    024-lista-spesa

---
+++ Esercitazioni/23-dizionario-dei-colori/Program.cs
@@ -0,0 +1,14 @@
+﻿// dizionario dei colori
+
+
+Dictionary<string, string> colori = new Dictionary<string, string>();
+colori.Add("rosso", "#FF0000");
+colori.Add("verde", "#00FF00");
+colori.Add("blue", "#0000FF");
+
+// ciclo che stampa chiavi e valori KeyValuePair rappresenta la coppia di valori
+
+foreach(KeyValuePair<string, string> colore in colori){
+    Console.WriteLine($"Il colore {colore.Key} ha il codice {colore.Value}");
+}
+
---
+++ Esercitazioni/24-lista-spesa/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Dictionary<string, int> listaSpesa = new Dictionary<string, int>();
+
+// aggiunge valore stringa e int 
+
+listaSpesa.Add("pane", 1);
+listaSpesa.Add("latte", 2);
+
+
+
+// aggiungere un nuovo articolo in modo alternativo
+
+listaSpesa["uova"] = 12;
+
+// incrementa la quantità di un articolo già presente
+
+listaSpesa["pane"] = listaSpesa["pane"] + 1;
+
+foreach(KeyValuePair<string, int> articolo in listaSpesa){
+    Console.WriteLine($"Articolo: {articolo.Key}, Quantità: {articolo.Value}");
+
+}
