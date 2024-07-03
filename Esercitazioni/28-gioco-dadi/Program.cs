53f924be4bd08f68abb839d3a2c24f39a06beea5
Neomatrix0 <danimusci@yahoo.it>
2024-07-03 11:38:30 +0200

    28-gioco-dadi

---
+++ Esercitazioni/28-gioco-dadi/Program.cs
@@ -0,0 +1,90 @@
+﻿// dice game
+Console.WriteLine("Lancio dei dadi");
+
+
+Random random = new Random();
+
+int scoreGiocatore1 = 100;
+
+int scorePC = 100;
+bool prosecuzione = true;
+
+do
+{
+    Console.Clear();
+
+    // primo lancio giocatore 1
+    int primoLancioGiocatore1 = random.Next(1, 7);
+
+    // secondo lancio giocatore 1
+
+    int secondoLancioGiocatore1 = random.Next(1, 7);
+
+    // somma dei 2 lanci del giocatore 1
+    int sommaLanciGiocatore1 = primoLancioGiocatore1 + secondoLancioGiocatore1;
+
+    Console.WriteLine($"\nIl giocatore 1 ha sorteggiato: {sommaLanciGiocatore1}");
+
+    // primo lancio pc
+    int primoLancioPC = random.Next(1, 7);
+
+    // secondo lancio pc
+
+    int secondoLancioPC = random.Next(1, 7);
+
+    // somma lanci pc
+    int sommaLancioPC = primoLancioPC + secondoLancioPC;
+
+    Console.WriteLine($"Il pc ha sorteggiato: {sommaLancioPC}");
+
+    // se giocatore 1 ha fatto un punteggio > del PC.il punteggio del pc verrà diminuito della differenza dei punteggi
+
+    if (sommaLanciGiocatore1 > sommaLancioPC)
+    {
+        int differenza = sommaLanciGiocatore1 - sommaLancioPC;
+        scorePC = scorePC - differenza;
+        Console.WriteLine($"score PC= {scorePC}");
+        Console.WriteLine($"score Giocatore1= {scoreGiocatore1}");
+
+        Console.WriteLine("\nPremi un tasto per proseguire il turno\n");
+        Console.ReadKey();
+
+        // se pc raggiunge lo 0 ha perso e il gioco finisce
+
+        if (scorePC <= 0)
+        {
+            Console.WriteLine($"\nPC ha perso");
+            prosecuzione = false;
+        }
+
+        // se lanci del pc sono > dei lanci del giocatore 1,  l giocatore1 vedrà diminuito il proprio punteggio della differenza tra i risultati
+    }
+    else if (sommaLanciGiocatore1 < sommaLancioPC)
+    {
+        int differenza2 = sommaLancioPC - sommaLanciGiocatore1;
+        scoreGiocatore1 = scoreGiocatore1 - differenza2;
+        Console.WriteLine($"score giocatore 1= {scoreGiocatore1}");
+        Console.WriteLine($"score PC= {scorePC}");
+        Console.WriteLine("Premi un tasto per proseguire il turno");
+        Console.ReadKey();
+
+        // se il punteggio di giocatore 1 è < di 0 giocatore 1 ha perso
+        if (scoreGiocatore1 <= 0)
+        {
+            Console.WriteLine($"\nGiocatore1 hai perso");
+            prosecuzione = false;
+        }
+    }
+
+    // parità
+
+    else
+    {
+        Console.WriteLine("parità");
+
+    }
+
+} while (prosecuzione);       // condizione di prosecuzione del ciclo
+
+
+
---
+++ Esercitazioni/28-gioco-dadi/README.md
@@ -0,0 +1,48 @@
+# DICE GAME
+
+Rappresentazione del flusso di gioco e delle decisioni che vengono prese a ogni turno
+
+- Ci sono 2 giocatori
+
+- Ognuno lancia 2 dadi
+
+- Entrambi i giocatori iniziano con 100 punti
+
+- Ogni turno ciascuno lancia 2 dadi, la differenza della somme dei punteggi dei dadi viene sottratta ai punti del giocatore con il totale minore
+
+> **Ad esempio** se il computer lancia 6 e il giocatore umano lancia 12 al computer vengono sottratti 6 punti dal punteggio di 100 aggiornato il punteggio a 94
+
+- il primo che arriva a 0 punti o meno perde
+
+- il gioco continua fino a quando il computer o il giocatore umano raggiungono 0 punti
+
+> **TO DO:**  Implementare la persistenza dei dati 
+
+
+```mermaid
+
+
+
+   flowchart LR
+
+
+   Start -->
+   Random-->
+   A[Player 1 lancio1] & C[PC lancio1]
+   A-->B[Player 1 lancio 2]-->F[somma lancio giocatore1]-->G
+   C-->D[PC lancio 2]-->E[somma lancio pc] -->
+   G{lancio giocatore1 maggiore di lancio pc?}--si-->H[Sottrai punti a pc per la differenza dei punteggi]-->L
+   G --no -->I[Sottrai punti a giocatore 1 per la differenza dei punteggi]-->
+   L{Chi perde? }-->M[se punteggo PC == 0]-->N[pc perde e vince giocatore 1]
+   L-->O[se punteggio giocatore1 =0 ]-->P[Giocatore1 perde vince pc]
+   L-->V[se entrambi hanno stesso punteggio = parità]-->Z
+   N & P-->Z[end]
+
+
+
+
+
+
+```
+
+
