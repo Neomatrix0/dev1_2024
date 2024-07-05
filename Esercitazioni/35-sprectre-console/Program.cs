293e3868ce78e54250dc8f15d31ebfa721049fbf
Neomatrix0 <danimusci@yahoo.it>
2024-07-05 16:00:17 +0200

    aggiunte lezione venerdi

--- Esercitazioni/35-sprectre-console/Program.cs
+++ Esercitazioni/35-sprectre-console/Program.cs
@@ -1,7 +1,10 @@
 ﻿// Examples of Spectre console
+using Spectre.Console;
 
 
-using Spectre.Console;  // importa il namespace della libreria Spectre.Console
+
+/*
+//using Spectre.Console;  // importa il namespace della libreria Spectre.Console
 
 
 AnsiConsole.WriteLine();
@@ -15,3 +18,58 @@ AnsiConsole.Write(new Calendar(2020, 10)
 
 // comando dotnet installazione dentro cartella progetto specifico
 // dotnet add package Spectre.Console
+
+
+
+
+*/
+
+
+AnsiConsole.Clear();
+
+
+// creare una scritta colorata
+
+AnsiConsole.Write(new Markup("[bold yellow]Hello[/] [bold red]World!\n[/]"));
+
+
+AnsiConsole.Write(new Markup("[28]Hello[/] [blue]World!\n[/]"));  
+
+
+
+// esempio di testo formattato
+
+AnsiConsole.Markup("[underline red]Hello[/] [10]World![/]");
+
+AnsiConsole.Markup("[30]Hello[/] [10]World![/]");
+
+AnsiConsole.WriteLine();
+
+var continua = AnsiConsole.Confirm("Vuoi continuare?");
+
+// esempio di tabella
+
+var table = new Table();
+table.AddColumn("Nome corso");
+table.AddColumn("Anno");
+table.AddRow("Corso di informatica","2024");
+AnsiConsole.Render(table);
+
+var rule = new Rule("[red]Hello[/]");
+rule.Justification = Justify.Left; // Justify.center, Justify.right
+AnsiConsole.Write(rule);
+
+// esempio di prompt
+
+var nome = AnsiConsole.Prompt(new TextPrompt<string>("Inserisci il tuo nome:"));
+
+// esempio di panel
+
+var panel = new Panel("Questo è un testo all'interno di un pannello");
+
+panel.Border = BoxBorder.Rounded;
+AnsiConsole.Render(panel);
+
+// esempio di progress bar
+
+
