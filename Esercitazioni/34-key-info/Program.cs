7b04ccf7cc0be17cbecf70434062a00d2d05dd67
Neomatrix0 <danimusci@yahoo.it>
2024-07-05 15:03:00 +0200

    modifiche

--- Esercitazioni/34-key-info/Program.cs
+++ Esercitazioni/34-key-info/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Ciclo che continua fino a quando non viene premuto il tasto 'N' utilizzando KeyInfo
+// keyInfo  è una struttura che rappresenta le informazioni di un tasto premuto sulla tastiera.
+// è case insensitive quindi 'N' e in 'n'sono considerati 2 tasti uguali
+// ad esempio keyInfo.Key == ConsoleKey.N significa che se premi N o n sulla tastiera e il ciclo termina
+// viene utilizzata per leggere i tasti premuti dall'utente senza mostrare i caratteri a schermo
+
+
+Console.WriteLine("Premi N per continuare...");
+
+
+        while (true)
+        {
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            if (keyInfo.Key == ConsoleKey.N) // se premo N
+            {
+                break; // Esce dal ciclo se viene premuto 'N'
+            }
+        }
+
+//imposta il colore dello sfondo e del testo
+
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.BackgroundColor = ConsoleColor.White;
+
+// stampa messaggio
+
+Console.WriteLine("Questo testo è bianco con sfondo blu");
+
+// resetta i colori
+
+Console.ResetColor();
+Console.WriteLine("Questo tasto è stampato con i colori predefiniti");
---
+++ Esercitazioni/35-sprectre-console/Program.cs
@@ -0,0 +1,17 @@
+﻿// Examples of Spectre console
+
+
+using Spectre.Console;  // importa il namespace della libreria Spectre.Console
+
+
+AnsiConsole.WriteLine();
+AnsiConsole.Write(new Calendar(2020, 10)
+     .RoundedBorder()
+     .HighlightStyle(Style.Parse("red"))
+     .HeaderStyle(Style.Parse("yellow"))
+     .AddCalendarEvent("An event", 2020, 9, 22)
+     .AddCalendarEvent("Another event", 2020, 10, 2)
+     .AddCalendarEvent("A third event", 2020, 10, 13));
+
+// comando dotnet installazione dentro cartella progetto specifico
+// dotnet add package Spectre.Console
