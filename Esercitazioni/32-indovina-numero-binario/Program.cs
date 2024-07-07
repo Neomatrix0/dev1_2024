b2a271bcfaeb19745c1823bf2e1e88f1ae9ee801
Neomatrix0 <danimusci@yahoo.it>
2024-07-07 17:45:23 +0200

    modifiche

--- Esercitazioni/32-indovina-numero-binario/Program.cs
+++ Esercitazioni/32-indovina-numero-binario/Program.cs
@@ -8,7 +8,7 @@
         int counter = 0;
         string userSuggest;
         bool guessed = false;
-        int mid = (minNumber + maxNumber) / 2;
+        
 
         Console.WriteLine($"PC: Welcome, please think of a number between {minNumber} and {maxNumber}...");
 
@@ -19,7 +19,7 @@
             counter++;
 
             // Guess the middle number
-            int guessNumber = mid;
+            int guessNumber = (minNumber+ maxNumber)/2;
 
             // Computer tries to guess the number
             Console.WriteLine($"PC: I chose the number {guessNumber}. Press a button to continue...");
@@ -35,23 +35,26 @@
                 case "c":
                     Console.WriteLine($"Congratulations! The game is over, you guessed the number in {counter} tries."); // win case
                     guessed = true;
+                    Thread.Sleep(1000);
                     break;
 
                 case "+":
                     // Update range for a higher number
                     minNumber = guessNumber + 1;
-                    mid = (minNumber + maxNumber) / 2;
+                    Thread.Sleep(1000);
                     break;
 
                 case "-":
                     // Update range for a lower number
                     maxNumber = guessNumber - 1;
-                    mid = (minNumber + maxNumber) / 2;
+                    Thread.Sleep(1000);
+                    
                     break;
 
                 default:
                     Console.WriteLine("Please enter a correct answer: + for higher, - for lower, c for correct."); // in case of wrong input
                     counter--; // reduce 1
+                    Thread.Sleep(1000);
                     break;
             }
         }
