608075b46cd352d6d4cd93a5d75e46d649de9313
Neomatrix0 <danimusci@yahoo.it>
2024-07-17 15:24:58 +0200

    aggiunte

---
+++ Esercitazioni/65-lettura-csv-scrittura-json/Program.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string path = @"test.csv";
+        string[] lines = File.ReadAllLines(path); // legge tutte le righe del file
+
+        string[][] prodotti = new string[lines.Length][]; // crea un array di array di stringhe con la lunghezza del numero di righe del file
+        for (int i = 1; i < lines.Length; i++)
+        {
+            prodotti[i] = lines[i].Split(',');  //assegna ad ogni elemento dell'array di array di stringhe il valore della riga corrispondente divisa in un array di stringhe ed utilizza la virgola come separatore
+        }
+        for (int i = 1; i < prodotti.Length; i++)
+        {
+            string path2 = prodotti[i][0] + ".json";    //crea i files utilizzando la chiave come nome
+            File.Create(path2).Close();                 // crea il file
+            File.AppendAllText(path2, JsonConvert.SerializeObject(new { nome = prodotti[i][0], prezzo = prodotti[i][1] })); //scrive la riga nel file
+        }
+    }
+}
---
+++ Esercitazioni/66-creazione-json-con-interfaccia/Program.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string path = @"test.json";
+        File.Create(path).Close();
+        File.AppendAllText(path, "[\n");
+
+        while(true){
+
+            Console.WriteLine("Inserisci nome e prezzo");
+            string nome = Console.ReadLine();
+            string prezzo = Console.ReadLine();
+
+            //File.AppendAllText(path, JsonConvert.SerializeObject(new {nome, prezzo})+ ",\n");  // scrive la riga nel file
+            //di seguito il metodo alternativo riga 19
+            //serializza l'oggetto con indentazione
+            string jsonString = JsonConvert.SerializeObject(new {nome,prezzo},Formatting.Indented);
+            File.AppendAllText(path, jsonString + ",\n");
+            Console.WriteLine("Vuoi inserire un altro prodotto?(s/n)");
+            string risposta = Console.ReadLine();
+            if(risposta == "n"){
+                break;
+            }
+        }
+
+        // togli ultima virgola dall'ultimo oggetto
+
+        string file = File.ReadAllText(path);
+        file = file.Remove(file.Length -2,1);   // gli argomenti -2 -1 indicano rispettivamente la posizione e il numero di caratteri da rimuovere dalla stringa
+        File.WriteAllText(path,file);
+        File.AppendAllText(path,"]");       // scrive la riga
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*if(!File.Exists(path)){
+            File.Create(path).Close();
+            File.AppendAllText(path, "[\n");
+        }
+
+        while(true){
+            Console.WriteLine("Inserisci nome:");
+            string nome = Console.ReadLine().Trim();
+
+            Console.WriteLine("Inserisci prezzo:");
+
+            if(decimal.TryParse(Console.ReadLine(),out decimal prezzo))
+        }
+*/
+
+
+
+
+
+
+
+
+
+    
