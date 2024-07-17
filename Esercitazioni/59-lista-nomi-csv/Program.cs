55cbdb5b935838ca8294ef7887ad805100e9bc95
Neomatrix0 <danimusci@yahoo.it>
2024-07-17 10:58:37 +0200

    aggiunta

---
+++ Esercitazioni/59-lista-nomi-csv/Program.cs
@@ -0,0 +1,21 @@
+﻿// esercizi persistenza dati utilizzando csv 
+// chiede all'utente nome,cognome,età andando a capo ogni volta
+
+string path = @"test.csv"; // in questo caso il file è nella stessa cartella del programma
+
+File.Create(path).Close();   // crea il file
+
+
+while(true){
+    Console.WriteLine("Inserisci nome,cognome,età");
+    string nome = Console.ReadLine();           // legge nome
+    string cognome = Console.ReadLine();        // legge cognome
+    string eta = Console.ReadLine();            // legge eta
+    File.AppendAllText(path,nome + "," + cognome + "," + eta + "\n"); // scrive la riga nel file
+    Console.WriteLine("Vuoi inserire un altro nome? (s/n)");
+    string risposta = Console.ReadLine();
+    if(risposta == "n"){                        // se n esce dal loop
+        break;
+    }
+    
+}
---
+++ Esercitazioni/60-lista-nomi-inserimento-csv/Program.cs
@@ -0,0 +1,81 @@
+﻿// esercizi persistenza dati utilizzando csv 
+// chiede all'utente nome,cognome,età andando a capo ogni volta
+
+ // in questo caso il file è nella stessa cartella del programma
+
+string path = @"test.csv";
+File.Create(path).Close(); 
+
+while (true)
+{
+    Console.WriteLine("Inserisci nome,cognome,età uno sotto l'altro");
+    string nome = Console.ReadLine();           // legge nome
+    string cognome = Console.ReadLine();        // legge cognome
+    string eta = Console.ReadLine();
+   string[]lines  = File.ReadAllLines(path);
+    bool found = false;
+
+foreach(string line in lines){
+    if(line.StartsWith(nome)){
+        found = true;
+        break;
+    }
+}
+if(!found){
+    File.AppendAllText(path,nome + "," + cognome +","+ eta + "\n");
+}else{
+    Console.WriteLine("Il nome è già presente nel file");
+}
+
+  Console.WriteLine("Vuoi inserire un altro nome? (s/n)");
+        string risposta = Console.ReadLine();
+        if (risposta == "n")
+        {
+            break;
+
+        }
+        }
+
+
+
+
+
+//List<string> nomi = new List<string>(File.ReadAllLines(path));
+  // crea il file
+
+
+//bool duplicato = false;   // crea un array di stringhe con la lunghezza del numero di righe del file
+
+
+/*
+
+while (true)
+{
+    Console.WriteLine("Inserisci nome,cognome,età");
+    string nome = Console.ReadLine();           // legge nome
+    string cognome = Console.ReadLine();        // legge cognome
+    string eta = Console.ReadLine();
+    string dati = ($"{nome},{cognome},{eta}");
+   
+
+    if (nomi.Contains(dati))
+    {
+
+        Console.WriteLine("Nome già esistente");
+    }
+    else
+    {
+
+         File.AppendAllText(path, dati + "\n");
+
+        Console.WriteLine("Vuoi inserire un altro nome? (s/n)");
+        string risposta = Console.ReadLine();
+        if (risposta == "n")
+        {
+            break;
+
+        }
+
+    }
+*/
+
