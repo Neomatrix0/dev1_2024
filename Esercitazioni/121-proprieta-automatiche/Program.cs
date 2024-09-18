11dc1f2fb686cd43cf18f9c7eaf9b5acb1eef89d
Neomatrix0 <danimusci@yahoo.it>
2024-09-18 09:35:40 +0200

    classi-proprieta-automatiche

---
+++ Esercitazioni/121-proprieta-automatiche/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Persona{
+    public string Nome{ get; set; }
+    public string Cognome{ get; set; }
+    public int Eta{ get; set; }
+}
+
+class Program{
+    public static void Main(string[] args){
+        Persona persona = new Persona{
+            Nome="Mario",
+            Cognome="Rossi",
+            Eta = 30
+        };
+
+        Console.WriteLine($"Nome: {persona.Nome}");
+         Console.WriteLine($"Cognome: {persona.Cognome}");
+          Console.WriteLine($"Eta: {persona.Eta}");
+
+          //chiudere la console
+
+          Console.ReadKey();
+    }
+}
+
+
+
