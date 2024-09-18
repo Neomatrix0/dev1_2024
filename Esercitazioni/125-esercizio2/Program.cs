1d9cecd9da5661cbf2cc97ead824881c618c0230
Neomatrix0 <danimusci@yahoo.it>
2024-09-18 10:59:41 +0200

    esempio-impiegato

---
+++ Esercitazioni/125-esercizio2/Program.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+// Definizione di una classe per mappare i dati JSON
+public class Persona{
+
+public string Nome { get; set;}
+    public int Eta { get; set;}
+    public bool Impiegato{ get; set;}
+    
+
+}
+
+public class GestioneJson{
+    public static void Main(string[] args){
+// creazione di un oggetto persona
+        Persona persona = new Persona(){
+            Nome = "Mario Rossi",
+            Eta = 30,
+            Impiegato = true
+        };
+
+        // serializzazione dell'oggetto in una stringa json
+
+        string json = JsonConvert.SerializeObject(persona,Formatting.Indented); //Formatting.Indented per formattare il JSON
+        File.WriteAllText(@"persona.json",json);
+
+        // Deserializzazione della stringa JSON in un oggetto Persona
+
+        string jsonDaLeggere = File.ReadAllText(@"persona.json");
+        Persona personaDeserializzata = JsonConvert.DeserializeObject<Persona> (jsonDaLeggere);
+
+        System.Console.WriteLine(personaDeserializzata.Nome);
+    }
+}
