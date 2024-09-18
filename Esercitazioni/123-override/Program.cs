d8e24e223255c0bda615cd7cb80f0b3d108d801e
Neomatrix0 <danimusci@yahoo.it>
2024-09-18 10:00:28 +0200

    override

--- Esercitazioni/123-override/Program.cs
+++ Esercitazioni/123-override/Program.cs
@@ -48,3 +48,23 @@ class Program{
 
           Console.ReadKey();
     }}
+
+
+    /*
+
+    // puoi definire costruttori per inizializzare le proprietà al momento della creazione dell'oggetto
+
+    public class Studente :Persona
+    {
+
+    public string Matricola{ get;set; }
+    public string CorsoDiStudi{get;set; }
+
+    //costruttore
+    public Studente(string nome,string cognome,int eta,string matricola,string corsoDiStudi){
+    Nome = nome;
+    Cognome = cognome;
+    Eta = eta;
+    Matricola = matricola;
+    CorsoDiStudi = corsoDiStudi;
+    }
