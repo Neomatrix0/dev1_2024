fa693215a5d81badf75f1609fbd7bfc50a72ea90
Neomatrix0 <danimusci@yahoo.it>
2024-10-14 15:18:59 +0200

    updates application3

--- Esercitazioni/128-Identity-webapp/Program.cs
+++ Esercitazioni/128-Identity-webapp/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using _128_Identity_webapp.Data;
+//using MvaAuthApp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -39,6 +40,37 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
+//Aggiungi script di seed
+
+using(var scope = app.Services.CreateScope()){
+    var services = scope.ServiceProvider;
+    var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
+    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+    await SeedAdminUser(userManager, roleManager);
+}
+
+//definizone metodo
+
+async Task SeedAdminUser(UserManager<IdentityUser> userManager,RoleManager<IdentityRole> roleManager){
+    if(!await roleManager.RoleExistsAsync("Admin")){
+        await roleManager.CreateAsync(new IdentityRole("Admin"));
+    }
+
+    // crea l'utente admin se esiste
+
+       if(await userManager.FindByEmailAsync("info2@admin.com")== null){
+        var user = new IdentityUser{
+            UserName = "info2@admin.com",
+            Email = "info2@admin.com",
+            EmailConfirmed = true,
+        };
+        var result = await userManager.CreateAsync(user,"Admin123!"); //imposta password utente Admin
+        if(result.Succeeded){
+            await userManager.AddToRoleAsync(user,"Admin"); //aggiungi l'utente admin al ruolo Admin
+        }
+    }
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -57,6 +89,10 @@ app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
+
+
+app.UseStatusCodePagesWithReExecute("/Home/Error");
+ //app.UseHsts();
  // Aggiungi l'autenticazione qui
 
 
