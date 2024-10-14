52218cdd2d76d6cfbb35063527d9f071d37ce27a
Neomatrix0 <danimusci@yahoo.it>
2024-10-14 11:18:57 +0200

    updates

---
+++ Esercitazioni/128-Identity-webapp/Program.cs
@@ -0,0 +1,78 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using _128_Identity_webapp.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>() // Aggiunge supporto per i ruoli
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Aggiungo ambito di servizio per la gestione dei ruoli
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        // Risolvi il RoleManager dal provider di servizi
+        var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+        // Chiamata al metodo per assicurare che i ruoli esistano
+        await ApplicationDbInitializer.EnsureRolesAsync(roleManager);
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Errore avvenuto durante la creazione dei ruoli");
+    }
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
+
+public static class ApplicationDbInitializer
+{
+    public static async Task EnsureRolesAsync(RoleManager<IdentityRole> roleManager)
+    {
+        var roles = new List<string> { "Admin", "User" };
+        foreach (var role in roles)
+        {
+            if (!await roleManager.RoleExistsAsync(role))
+            {
+                await roleManager.CreateAsync(new IdentityRole(role)); // Corretto typo qui
+            }
+        }
+    }
+}
